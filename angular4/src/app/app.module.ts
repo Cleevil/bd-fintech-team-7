ff6787d533fc5210750eeecd076f0c94f32013db
Cleevil <cleevil@users.noreply.github.com>
2018-04-19 18:53:39 +0200

    added new page

--- angular4/src/app/app.module.ts
+++ angular4/src/app/app.module.ts
@@ -9,21 +9,24 @@ import { ChartModule } from 'angular2-highcharts';
 import { HighchartsStatic } from 'angular2-highcharts/dist/HighchartsService';
 import * as highcharts from 'highcharts';
 
-import { Service } from '../services/service';
-import { AppComponent } from './app.component';
-import { Front } from '../pages/front/front';
-import { Transactions } from '../pages/transactions/transactions';
-import { Overview } from '../pages/overview/overview';
-import { Bankbot } from '../pages/bankbot/bankbot';
-import { SecretDirective } from './secret.directive';
+import { Service           } from '../services/service'                   ;
+import { AppComponent      } from './app.component'                       ;
+import { Front             } from '../pages/front/front'                  ;
+import { Transactions      } from '../pages/transactions/transactions'    ;
+import { TransactionsByCat } from '../pages/transactionsByCat/transactions';
+import { Overview          } from '../pages/overview/overview'            ;
+import { Bankbot           } from '../pages/bankbot/bankbot'              ;
+import { SecretDirective   } from './secret.directive'                    ;
 
 export const routes: Routes = [
-  { path: '', component: Front },
-  { path: 'transactions', component: Transactions},
-  { path: 'transactions/:id', component: Transactions },
-  { path: 'bankbot', component: Bankbot },
-  { path: 'overview', component: Overview }
-];
+  { path: ''                      , component: Front             },
+  { path: 'transactions'          , component: Transactions      },
+  { path: 'transactions/:id'      , component: Transactions      },
+  { path: 'transactionsbycat'     , component: TransactionsByCat },
+  { path: 'transactionsbycat/:id' , component: TransactionsByCat },
+  { path: 'bankbot'               , component: Bankbot           },
+  { path: 'overview'              , component: Overview          }
+  ];
 
 export function highchartsFactory() {
   return highcharts;
@@ -33,6 +36,7 @@ export function highchartsFactory() {
   declarations: [
     Front,
     Transactions,
+    TransactionsByCat,
     Overview,
     Bankbot,
     AppComponent,
---
+++ angular4/src/pages/transactionsByCat/transactions.ts
@@ -0,0 +1,77 @@
+import { Component } from '@angular/core';
+import { Service } from '../../services/service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+    templateUrl: './transactions.html'
+})
+export class TransactionsByCat {
+
+    private paramsSubscription;
+    private transactions = [];
+    private accounts = [];
+    private selectedAccount;
+    private currentPage;
+
+    constructor(
+        private service: Service,
+        private route: ActivatedRoute
+    ) { 
+        this.currentPage = 1;
+    }
+
+    ngOnInit() {
+        this.service.fetchAccounts().then(
+            data => {
+                this.accountDataReady(data);
+            }
+        )
+    }
+
+    private scrollToTop() {
+        document.body.scrollTo(0,0);
+    }
+
+    private accountDataReady(data){
+       this.accounts = data.accounts;
+       this.paramsSubscription = this.route.params.subscribe(
+            params => {
+                for(let account of this.accounts){
+                    if(account.account_nbr === params['id']){
+                        this.selectAccount(account);
+                        break;
+                    }
+                }
+            }
+        );
+    }
+
+    private selectAccount(account){
+        this.selectedAccount = account;
+        this.selectedAccount.balance = account.balance;
+        this.fetchTransactionData(this.selectedAccount, this.currentPage);
+    }
+
+    private fetchTransactionData(account, page){
+        this.service.fetchTransactionsByPage(account.account_nbr, page)
+        .then(data => {
+            this.transactions = data['transactions'];
+        })
+    }
+
+    private getMoreTransactions(){
+        this.currentPage++;
+        this.service.fetchTransactionsByPage(this.selectedAccount.account_nbr, this.currentPage)
+        .then(data => {
+            this.transactions = this.transactions.concat(data['transactions']);
+        });
+    }
+
+    private isAccountSelected(account){
+        return account.account_nbr == this.selectedAccount.account_nbr;
+    }
+
+    ngOnDestroy() {
+        this.paramsSubscription.unsubscribe();
+    }
+}
